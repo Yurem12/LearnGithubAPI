03d0e6aa7d475c3ab13d90ec40413dbf00b0f3ab
Yurem12 <84666021+Yurem12@users.noreply.github.com>
2022-05-21 17:09:03 +0200

    stats + location update
    permet d'afficher compteur d'abonnés et d'abonnements en plus que de définir en tant qu': "inconnue" la localisation si elle n'est pas spécifiée.

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -10,7 +10,7 @@ $(
             })
             .done(function(data) {
                     console.log(`le nom d'utilisateur est: ${data.login}, ce développeur est localisé dans le pays: ${data.location}. Sa biographie est: ${data.bio}`);
-                    var bio = $(`<p>${data.bio}</p>`);
+                    var bio = $(`<p>Abonnés: ${data.followers} Abonnements: ${data.following}. ${data.bio}</p>`);
                     if(data.login === data.name) {
                         var username = $(`<h1>${data.name}</h1>`);
                     } else {
@@ -44,18 +44,20 @@ $(
             })
             .done(function(data) {
                     console.log(`le nom d'utilisateur est: ${data.login}, ce développeur est localisé dans le pays: ${data.location}. Sa biographie est: ${data.bio}`);
-                    var bio = $(`<p>${data.bio}</p>`);
+                    var bio = $(`<p>Abonnés: ${data.followers} Abonnements: ${data.following}. ${data.bio}</p>`);
                     if(data.login === data.name) {
                         var username = $(`<h1>${data.name}</h1>`);
                     } else {
                         var username = $(`<h1>${data.name} / ${data.login}</h1>`);
                     }
                     if (data.location == null) {
+                        var location = $(`<li>Inconnue</li>`);
+                    } else {
                         var location = $(`<li>${data.location}</li>`);
                     }
                     var avatar = $(`<img src="${data.avatar_url}"></img>`);
                     $(`#userDisplay`).append(username, avatar, location, bio);
-            })
+            });
             $.ajax({
                 method: "GET",
                 url: `https://api.github.com/users/${formField}/repos`,
@@ -68,10 +70,10 @@ $(
                     $(div).append(repoName, langage);
                     $(`#userDisplay`).append(div);
                 });
-            })
+            });
         }
 
         affichageFixe();
-        $("#submit").on('click', affichageDynamique)
+        $("#submit").on('click', affichageDynamique);
     }
-)
+);
